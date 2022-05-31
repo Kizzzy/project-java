77e6d4f8b903ef1035b1ab8420fd1fbe49c3e324
Kizzzy <xuhua0174@163.com>
2022-06-01 07:40:52 +0800

    [toolkit-java] * update module

--- kernel_git.bat
+++ kernel_git.bat
@@ -1,5 +1,50 @@
 @set current=%~dp0
-@echo current
-@for %%i in () do @cd /d %current%%%i&%~1
+
+@echo %current%
+@echo %~1
+
+@for %%i in (
+    game-2048
+    game-gobang
+    game-tetris
+    toolkit-21tb
+    toolkit-clicker
+    toolkit-codec
+    toolkit-crawler
+    toolkit-data-analysis
+    toolkit-db-mysql
+    toolkit-db-xlsx
+    toolkit-dnf
+    toolkit-ffmpeg
+    toolkit-file-hash
+    toolkit-file-merge
+    toolkit-file-rename
+    toolkit-gui
+    toolkit-hls
+    toolkit-http
+    toolkit-image-blend
+    toolkit-json
+    toolkit-krkr
+    toolkit-memory
+    toolkit-misc
+    toolkit-mobile
+    toolkit-pfs
+    toolkit-pkg
+    toolkit-proxy
+    toolkit-psw-get
+    toolkit-qqfo
+    toolkit-qqnc
+    toolkit-qqt
+    toolkit-quickly
+    toolkit-renpy
+    toolkit-sanguo-fuzhu
+    toolkit-sanguo-shop
+    toolkit-sghero
+    toolkit-shikari
+    toolkit-signer
+    toolkit-swf
+    toolkit-tth
+    toolkit-viewer
+    toolkit-window) do @cd /d %current%%%i&%~1
 
 @pause
---
+++ package-release.bat
@@ -0,0 +1,8 @@
+@call package.bat toolkit-dnf
+@call package.bat toolkit-pfs
+@call package.bat toolkit-pkg
+@call package.bat toolkit-renpy
+@call package.bat toolkit-qqfo
+@call package.bat toolkit-qqt
+@call package.bat toolkit-shikari
+@call package.bat toolkit-viewer
