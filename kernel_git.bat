b061b2efb256f36270e74b66753ae099388056b7
Kizzzy <xuhua0174@163.com>
2022-08-18 20:35:53 +0800

    [lib47-java] + add archive, free-arc, hxdl, media module

--- kernel_git.bat
+++ kernel_git.bat
@@ -1,14 +1,21 @@
 @set current=%~dp0
-@echo current
+
+@echo %current%
+@echo %~1
+
 @for %%i in (
+    lib47-21tb
     lib47-algorithm
     lib47-animations
+    lib47-archive
     lib47-cmd
     lib47-dbtoolkit
     lib47-dnf
+    lib47-free-arc
     lib47-helper
     lib47-hls
     lib47-http
+    lib47-hxdl
     lib47-image
     lib47-io
     lib47-javafx
@@ -18,6 +25,7 @@
     lib47-json
     lib47-krkr
     lib47-manager
+    lib47-media
     lib47-netty
     lib47-network
     lib47-nim
