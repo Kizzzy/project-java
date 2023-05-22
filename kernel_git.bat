9fc865d342e5d433716a1d1b14fec68e0eb21566
Kizzzy <xuhua0174@163.com>
2023-05-22 14:01:53 +0800

    [toolkit-java] * update pom.xml、kernel_git.bat and kernel_mvn.bat

--- kernel_git.bat
+++ kernel_git.bat
@@ -1,57 +1,9 @@
 @set current=%~dp0
 
-@echo %current%
-@echo %~1
+@echo wsp: %current%
+@echo cmd: %~1
+@echo=
 
-@for %%i in (
-    game-2048
-    game-gobang
-    game-tetris
-    toolkit-21tb
-    toolkit-archive
-    toolkit-clicker
-    toolkit-codec
-    toolkit-crawler
-    toolkit-data-analysis
-    toolkit-db-mysql
-    toolkit-db-xlsx
-    toolkit-dnf
-    toolkit-dragonbones
-    toolkit-ffmpeg
-    toolkit-file-hash
-    toolkit-file-merge
-    toolkit-file-rename
-    toolkit-gui
-    toolkit-hls
-    toolkit-http
-    toolkit-hxdl
-    toolkit-image-blend
-    toolkit-json
-    toolkit-krkr
-    toolkit-media
-    toolkit-memory
-    toolkit-misc
-    toolkit-mobile
-    toolkit-pfs
-    toolkit-proxy
-    toolkit-psw-get
-    toolkit-pvz
-    toolkit-qq3g
-    toolkit-qqfo
-    toolkit-qqhx
-    toolkit-qqnc
-    toolkit-qqt
-    toolkit-quickly
-    toolkit-renpy
-    toolkit-sanguo-fuzhu
-    toolkit-sanguo-shop
-    toolkit-sghero
-    toolkit-shikari
-    toolkit-signer
-    toolkit-spine
-    toolkit-swf
-    toolkit-tth
-    toolkit-viewer
-    toolkit-window) do cd /d %current%%%i&if exist ".git" %~1
+@for /d %%i in (*) do @( cd /d %current%%%i & if exist ".git" echo %current%%%i & %~1 & echo= & cd /d %current% )
 
 @pause
