44b38fbc6816d8d716d67b7a0afd08727982f121
Kizzzy <xuhua0174@163.com>
2023-05-22 12:52:50 +0800

    [lib47-java] * update pom.xml and kernel_git.bat

--- kernel_git.bat
+++ kernel_git.bat
@@ -1,59 +1,9 @@
 @set current=%~dp0
 
-@echo %current%
-@echo %~1
+@echo wsp: %current%
+@echo cmd: %~1
+@echo=
 
-@for %%i in (
-    lib47-21tb
-    lib47-algorithm
-    lib47-animations
-    lib47-archive
-    lib47-cmd
-    lib47-dbtoolkit
-    lib47-dnf
-    lib47-dragonbones
-    lib47-free-arc
-    lib47-helper
-    lib47-hls
-    lib47-http
-    lib47-hxdl
-    lib47-image
-    lib47-io
-    lib47-iso
-    lib47-javafx
-    lib47-javafx-display
-    lib47-javafx-viewer
-    lib47-jnative
-    lib47-json
-    lib47-krkr
-    lib47-manager
-    lib47-media
-    lib47-netty
-    lib47-network
-    lib47-nim
-    lib47-pfs
-    lib47-poi
-    lib47-proxy
-    lib47-pvz
-    lib47-qq3g
-    lib47-qqfo
-    lib47-qqhx
-    lib47-qqt
-    lib47-qqxl
-    lib47-renpy
-    lib47-sanguo
-    lib47-sghero
-    lib47-shikari
-    lib47-spine
-    lib47-swf
-    lib47-taobao
-    lib47-task
-    lib47-temp
-    lib47-tencent
-    lib47-tencent-network
-    lib47-test
-    lib47-toolkit
-    lib47-vfs
-    lib47-vfs-network) do cd /d %current%%%i&if exist ".git" %~1
+@for /d %%i in (*) do @( cd /d %current%%%i & if exist ".git" echo %current%%%i & %~1 & echo= & cd /d %current% )
 
 @pause
