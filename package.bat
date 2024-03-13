5950a46d5d2b5ee80171719e6fc72c37f6765613
Kizzzy <xuhua0174@163.com>
2024-03-13 16:33:15 +0800

    [toolkit-java] * simplify .gitignore [toolkit-java] * use environment variable in package.bat

--- package.bat
+++ package.bat
@@ -1,2 +1,6 @@
-@set d=%date:~0,4%%date:~5,2%%date:~8,2%
-@call kernel_mvn.bat "mvn clean package -DskipTests -Dtoolkit.deploy.root=C:/Kizzzy/Toolkit -Dtoolkit.release.root=D:/DevProject/toolkit/toolkit-java-release/%d%" %~1
+@set folder=%date:~0,4%%date:~5,2%%date:~8,2%
+@set tdr=%CUSTOM_TOOLKIT_DEPLOY_ROOT%
+@set trr=%CUSTOM_TOOLKIT_RELEASE_ROOT%
+@set trr_full=%trr%/%folder%
+
+@call kernel_mvn.bat "mvn clean package -DskipTests -Dtoolkit.deploy.root=%tdr% -Dtoolkit.release.root=%trr_full%" %~1
