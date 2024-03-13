a3a18b80c4066e47a3e6c8fe792cf9df38e9055e
Kizzzy <xuhua0174@163.com>
2024-03-13 16:48:08 +0800

    [toolkit-java] * validate tdr and trr in package.bat

--- package.bat
+++ package.bat
@@ -3,4 +3,16 @@
 @set trr=%CUSTOM_TOOLKIT_RELEASE_ROOT%
 @set trr_full=%trr%/%folder%
 
+@if "%tdr%" == "" (
+    @echo The deploy root of toolkit is empty, please check the environment variable: CUSTOM_TOOLKIT_DEPLOY_ROOT
+    @pause
+    @exit
+)
+
+@if "%trr%" == "" (
+    @echo The release root of toolkit is empty, please check the environment variable: CUSTOM_TOOLKIT_RELEASE_ROOT
+    @pause
+    @exit
+)
+
 @call kernel_mvn.bat "mvn clean package -DskipTests -Dtoolkit.deploy.root=%tdr% -Dtoolkit.release.root=%trr_full%" %~1
